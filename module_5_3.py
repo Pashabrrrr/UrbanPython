0f504dbbf96a1b0a8f31e9054ef7d1eee96a5da6
Pashabrrrr <p_bazarov@mail.ru>
2024-12-19 00:28:06 +0500

    module_5_3

---
+++ module_5_3.py
@@ -0,0 +1,75 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name},  Кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return False
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+house1 = House('ЖК Крылья', 16)
+house2 = House('ЖК Светлый', 18)
+
+print(house1)
+print(house2)
+
+print(house1 == house2)  # False
+print(house1 < house2)   # True
+print(house1 <= house2)  # True
+print(house1 > house2)   # False
+print(house1 >= house2)  # False
+print(house1 != house2)  # True
+
+house1 += 2
+print(house1)  # Кол-во этажей должно стать 18
